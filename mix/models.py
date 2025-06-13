d0375495e305176d92481d5d445b13aa01b475d6
ArthurVieira-eng <arthurv.o.2507@gmail.com>
2025-06-13 20:47:12 -0300

    Update models.py

--- mix/models.py
+++ mix/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     detalhes = models.TextField()
+    image = models.ImageField(upload_to='produtos/')
     def __str__(self):
         return self.nome
 
